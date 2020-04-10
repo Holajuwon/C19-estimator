a5a7c24c4c5e05e45f921e04fceece14f87d0136
Holajuwon <henrijay00@gmail.com>
2020-04-10 15:35:13 +0100

    ft-add-log-file

---
+++ src/routes/logs.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  const filePath = path.resolve('./src/config/logger.txt');
+  try {
+    const data = fs.readFileSync(filePath, 'utf8');
+    res.send(data);
+  } catch (e) {
+    res.send('Error:', e.stack);
+  }
+});
+
+export default router;
