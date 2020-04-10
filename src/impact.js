a8b9be2676b6fd1b3e5d52eac6023a1acfe663fb
Holajuwon <henrijay00@gmail.com>
2020-04-10 02:30:52 +0100

    ft-challenge2-completed

--- src/impact.js
+++ src/impact.js
@@ -1,9 +1,14 @@
 export default (data, factor) => {
   const currentlyInfected = data.reportedCases * 10;
   const infectionsByRequestedTime = currentlyInfected * 2 ** factor;
+  const severeCasesByRequestedTime = infectionsByRequestedTime * 0.15;
+  const hospitalBedsByRequestedTime = Math.floor(data.totalHospitalBeds * 0.35)
+  - severeCasesByRequestedTime;
 
   return {
     currentlyInfected,
-    infectionsByRequestedTime
+    infectionsByRequestedTime,
+    severeCasesByRequestedTime,
+    hospitalBedsByRequestedTime
   };
 };
