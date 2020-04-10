8d450aca78e1dfefd8788e519242fabbe099b9b4
Holajuwon <henrijay00@gmail.com>
2020-04-10 04:49:25 +0100

    ch-fix-challenge3-with-new-update

--- src/estimate.js
+++ src/estimate.js
@@ -3,22 +3,22 @@ export default (data, currentlyInfected) => {
   let days;
   if (data.periodType === 'days') {
     days = data.timeToElapse;
-    factor = Math.floor(data.timeToElapse / 3);
+    factor = Math.trunc(data.timeToElapse / 3);
   } else if (data.periodType === 'weeks') {
     days = data.timeToElapse * 7;
-    factor = Math.floor((data.timeToElapse * 7) / 3);
+    factor = Math.trunc((data.timeToElapse * 7) / 3);
   } else if (data.periodType === 'months') {
     days = data.timeToElapse * 30;
-    factor = Math.floor((data.timeToElapse * 30) / 3);
+    factor = Math.trunc((data.timeToElapse * 30) / 3);
   }
 
   const infectionsByRequestedTime = currentlyInfected * 2 ** factor;
-  const severeCasesByRequestedTime = Math.floor(infectionsByRequestedTime * 0.15);
-  const hospitalBedsByRequestedTime = Math.ceil(data.totalHospitalBeds * 0.35
+  const severeCasesByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.15);
+  const hospitalBedsByRequestedTime = Math.trunc(data.totalHospitalBeds * 0.35
   - severeCasesByRequestedTime);
-  const casesForICUByRequestedTime = Math.floor(infectionsByRequestedTime * 0.05);
-  const casesForVentilatorsByRequestedTime = Math.floor(infectionsByRequestedTime * 0.02);
-  const dollarsInFlight = Math.floor(infectionsByRequestedTime
+  const casesForICUByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.05);
+  const casesForVentilatorsByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.02);
+  const dollarsInFlight = Math.trunc(infectionsByRequestedTime
   * data.region.avgDailyIncomePopulation
    * data.region.avgDailyIncomeInUSD * days);
 
